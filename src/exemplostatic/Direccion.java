7f53e6c64290504a418bc1fd0a91b0ff51a32df7
Samuel <Samuel@DESKTOP-V8Q12DA>
2018-10-25 09:39:42 +0200

    Hacer que se pase de una clase a otra y usarla llamando a la segunda.

---
+++ src/exemplostatic/Direccion.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplostatic;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Direccion {
+
+    private String telefono;
+    private String email;
+
+    public void setTelefono(String a) {
+        this.telefono = a;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setEmail(String b) {
+        this.email = b;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+}
